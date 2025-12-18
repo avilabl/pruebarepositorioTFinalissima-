d27175eb23303ceed8a7aa111112b8652d036288
Avila Brian Leonel <61570098+avilabl@users.noreply.github.com>
2025-12-18 11:08:41 -0300

    Update db.js

--- APIv1/db.js
+++ APIv1/db.js
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  waitForConnections: false,
+  waitForConnections: true,
   connectionLimit: 5,
   queueLimit: 0
 });
