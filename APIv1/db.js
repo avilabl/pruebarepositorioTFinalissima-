33de0590cc7f96b95eb22609dde7674fa4525f0f
Avila Brian Leonel <61570098+avilabl@users.noreply.github.com>
2025-12-18 11:21:57 -0300

    Increase database connection limit from 5 to 10

--- APIv1/db.js
+++ APIv1/db.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   waitForConnections: true,
-  connectionLimit: 5,
+  connectionLimit: 10,
   queueLimit: 0
 });
 
